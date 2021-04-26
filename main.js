c7a7173cd0d4b239d83be197e41ac73d7aa80fed
Stanley <stanleychen8@tamu.edu>
2021-04-26 17:25:02 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("enter your array please! (add a space between two numbers) ", function(list_of_numbers) {
+
+    console.log(`${list_of_numbers}`);
+    rl.close();
+});
+
+rl.on("close", function() {
+    
+    process.exit(0);
+});
