3038f656e9f337a823dba7dd8541a6d8f62c7b59
Stanley <stanleychen8@tamu.edu>
2021-04-26 18:50:55 -0500

    formatted input
    separated numbers and added them to a new array

--- main.js
+++ main.js
@@ -4,14 +4,29 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+var nums = [];
+
 rl.question("enter your array please! (add a space between two numbers) ", function(list_of_numbers) {
+	temp_string = "";
+	for (var i = 0; i < list_of_numbers.length; i++){
+		if (list_of_numbers[i] == ' '){
+			nums.push(temp_string);
+			temp_string = "";
+		} else {
+			temp_string += list_of_numbers[i];
+		}
+	}
+	nums.push(temp_string);
+
+	for (var i = 0; i < nums.length; i++){
+		console.log(nums[i]);
+	}
 
     console.log(`${list_of_numbers}`);
-    
+
     rl.close();
 });
 
 rl.on("close", function() {
-    
     process.exit(0);
 });
