1d3907d1fed1663fc5beec47d77977f9bb4d414c
Stanley <stanleychen8@tamu.edu>
2021-04-26 19:03:43 -0500

    finished
    takes a list of numbers, outputs the word-version

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const rl = readline.createInterface({
 var nums = [];
 
 rl.question("enter your array please! (add a space between two numbers) ", function(list_of_numbers) {
+	var dict = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+	output_string = "";
 	temp_string = "";
 	for (var i = 0; i < list_of_numbers.length; i++){
 		if (list_of_numbers[i] == ' '){
@@ -18,11 +20,21 @@ rl.question("enter your array please! (add a space between two numbers) ", funct
 	}
 	nums.push(temp_string);
 
+	
 	for (var i = 0; i < nums.length; i++){
-		console.log(nums[i]);
+		temp_number = "";
+		for (var j = 0; j < nums[i].length; j++){
+			temp_number += dict[nums[i][j] - '0'];
+		}
+		temp_number += ",";
+		output_string += temp_number;
+		temp_number = "";
 	}
+	sz = output_string.length;
+	output_string = output_string.substring(0, sz-1)
+	console.log(output_string);
 
-    console.log(`${list_of_numbers}`);
+    // console.log(`${list_of_numbers}`);
 
     rl.close();
 });
