8b2eb5f542b94497d90c1749fe95a7eadc6d9e22
Stanley <stanleychen8@tamu.edu>
2021-04-26 20:50:15 -0500

    Final submission
    Arguments can be taken the same line as shown in the instructions.
    Comments added

--- main.js
+++ main.js
@@ -1,44 +1,38 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+
+
 
 var nums = [];
-
-rl.question("enter your array please! (add a space between two numbers) ", function(list_of_numbers) {
-	var dict = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
-	output_string = "";
-	temp_string = "";
-	for (var i = 0; i < list_of_numbers.length; i++){
-		if (list_of_numbers[i] == ' '){
-			nums.push(temp_string);
-			temp_string = "";
-		} else {
-			temp_string += list_of_numbers[i];
-		}
-	}
-	nums.push(temp_string);
-
-	
-	for (var i = 0; i < nums.length; i++){
-		temp_number = "";
-		for (var j = 0; j < nums[i].length; j++){
-			temp_number += dict[nums[i][j] - '0'];
-		}
-		temp_number += ",";
-		output_string += temp_number;
-		temp_number = "";
-	}
-	sz = output_string.length;
-	output_string = output_string.substring(0, sz-1)
-	console.log(output_string);
-
-    // console.log(`${list_of_numbers}`);
-
-    rl.close();
+//processed input
+process.argv.forEach(function (val, index, array) {
+  	if (index > 1){
+  		nums.push(val);
+  	}
 });
 
-rl.on("close", function() {
-    process.exit(0);
-});
+var dict = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+output_string = "";
+
+//conversion
+for (var i = 0; i < nums.length; i++){
+	temp_number = "";
+	for (var j = 0; j < nums[i].length; j++){
+		temp_number += dict[nums[i][j] - '0'];
+	}
+	temp_number += ",";
+	output_string += temp_number;
+	temp_number = "";
+}
+
+//trim the last comma
+sz = output_string.length;
+output_string = output_string.substring(0, sz-1)
+//answer
+console.log(output_string);
+
+
+
+
+
+
+
+
